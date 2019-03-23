96d89729de5edfca82f658cf6a4dce2b84e15be2
lucasoz <luemunozmo@unal.edu.co>
2019-03-23 18:45:42 -0500

    Bonus listo

---
+++ cursos.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        nombre: "Cisco CCNA 1",
+        duracion: 120,
+        valor: 700000
+    },
+    {
+        id: 2,
+        nombre: "Scala",
+        duracion: 150,
+        valor: 35000000
+    },
+    {
+        id: 3,
+        nombre: "Java 8",
+        duracion: 96,
+        valor: 1320000
+    }
+]
---
+++ express.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import cursos from './cursos'
+
+import yargs from 'yargs'
+
+const options = {
+  id: {
+    demand: true,
+    alias: 'i'
+  },
+  nombre: {
+    demand: true,
+    alias: 'n'
+  },
+  cedula: {
+    demand: true,
+    alias: 'c'
+  },
+}
+
+const imprimir =  (indice) => {
+  if (indice < cursos.length) {
+    let curso = cursos[indice]
+    setTimeout(() => {
+      console.log("El curso de "+
+      curso.nombre+" tiene una duración de "+
+      curso.duracion+" y cuesta $"+curso.valor+" y el id es "+curso.id)
+      imprimir(indice+1)
+    }, 2000)
+  }
+}
+
+const argumentos = yargs.command('inscribir', 'Inscribirme en un curso', options).argv
+
+let texto = 'Esperando inscripción'
+
+if (argumentos.i) {
+  let curso = cursos.find((curso) => {
+    return curso.id == argumentos.i
+  })
+  if (curso) {
+    texto = "El estudiante " + argumentos.n + " con cédula " + argumentos.c + "\n" +
+    " esta matriculado en el curso de "+ curso.nombre+" tiene una duración de "+ "\n" +
+    curso.duracion+" y cuesta $"+curso.valor
+  } else {
+    console.log("El curso seleccionado no existe intente de nuevo.");
+    imprimir(0)
+  }
+}else{
+  imprimir(0)
+}
+
+
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send(texto)
+})
+ 
+app.listen(3000)
